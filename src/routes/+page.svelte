45e31f58c65233494dcd895714e607aeb5c123db
MC <matty@mattycollins.com.au>
2022-12-21 01:12:31 +1100

    port and update "home" page from jekyll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	<b>Oh hi! I am Matty Collins</b>, a senior developer with over 10 years experience building
+	accessible websites and web apps.
+</p>
