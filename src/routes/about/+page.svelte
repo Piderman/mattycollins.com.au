85704568a320448b4b8213f98e27b058bf22a08b
MC <matty@mattycollins.com.au>
2022-12-21 01:08:10 +1100

    port and update "about" page from jekyll

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,58 @@
+<p>
+	I am a senior developer with over 10 years experience building accessible websites and web apps.
+	Specialising in front-end development I have been focusing on Jamstackfor the past few years.
+</p>
+
+<p>
+	I completed a degree in Games and Interactivity (Arts) in 2010, with subjects scattered across the
+	history of media in Australia, 3D modeling and animation, game design, and web development. I have
+	dabbled in web for a long time, starting with table layouts and Frontpage and like most people, I
+	got into web because my band needed a website. But I really took to it after learning about
+	semantics and what you can do with CSS.
+</p>
+
+<h2>Work</h2>
+<ul>
+	<li>
+		2011-2014: Mintleaf / Websilk
+		<ul>
+			<li>junior front-end developer</li>
+			<li>lead web developer</li>
+		</ul>
+
+		Clients
+		<ul>
+			<li>vision australia</li>
+			<li>beyondblue</li>
+		</ul>
+	</li>
+	<li>
+		2014-2015: Soul Digital
+		<ul>
+			<li>front-end developer</li>
+		</ul>
+	</li>
+	<li>
+		2015-2022: isobar / Merkle
+		<ul>
+			<li>front-end developer</li>
+			<li>senior developer</li>
+		</ul>
+		Clients
+		<ul>
+			<li>MCG/MCC</li>
+			<li>Auspost</li>
+			<li>Jetstar</li>
+			<li>Holden</li>
+			<li>Swinburne University</li>
+			<li>Funlab</li>
+			<li>Mitsubishi Motors</li>
+		</ul>
+	</li>
+	<li>
+		2022: Southpaw studio
+		<ul>
+			<li>senior developer</li>
+		</ul>
+	</li>
+</ul>
